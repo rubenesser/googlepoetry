d66616f51937d1e6c07326e73534076ad64aeede
rubenesser <mail@rubenesser.nl>
2017-07-17 21:49:50 +0200

    put in karaoke bar

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
 //const button = document.querySelector('button');
 
+// let timecode = 0
+
+
+
+// This function set the images in the timecode.
 function setImagesInTime() {
   const images =
   [
-    { timecode: 1000, imgUrl: 'assets/images/my-life_01.png' },
-    { timecode: 2000, imgUrl: 'assets/images/my-life_02.jpeg' },
-    { timecode: 3000, imgUrl: 'assets/images/my-life_03.jpg' },
-    { timecode: 4000, imgUrl: 'assets/images/my-life_04.jpg' },
+    { timecode: 1150, imgUrl: 'assets/images/my-life_01.png', lineId: 'line1' },
+    { timecode: 1420, imgUrl: 'assets/images/my-life_02.jpeg', lineId: 'line1' },
+    { timecode: 2500, imgUrl: 'assets/images/had_stood_01.jpg', lineId: 'line2' },
+    { timecode: 2800, imgUrl: 'assets/images/had_stood_03.jpg', lineId: 'line2' },
+    { timecode: 3500, imgUrl: 'assets/images/loaded_gun_01.jpeg' },
+    { timecode: 3800, imgUrl: 'assets/images/loaded_gun_02.jpeg' },
+    { timecode: 4200, imgUrl: 'assets/images/loaded_gun_03.jpeg' },
+    { timecode: 5000, imgUrl: 'assets/images/in_corners_01.jpg' },
+    { timecode: 5800, imgUrl: 'assets/images/in_corners_02.jpg' },
+    { timecode: 7000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 8000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 9000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 10000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 11000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 12000, imgUrl: 'assets/images/had_stood_03.jpg' },
+    { timecode: 13000, imgUrl: 'assets/images/had_stood_03.jpg' },
   ]
 
+// This is the loop runs the timecode.
   for (const image of images) {
-    window.setTimeout(function() {
-      setImageInTime(image)
-    }, image.timecode)
+    window.setTimeout( function(){setImageInTime (image)}, image.timecode)
   }
 }
 
+//
 function setImageInTime(image) {
   const para = document.querySelector('p');
+  const line = document.getElementById(image.lineId)
+  const greenBackground = document.getElementsByClassName('green-background')
+
+  for (const i of greenBackground) {
+    i.className = '';
+  }
+
+  line.className = 'green-background';
   para.innerHTML = '<img src="' + image.imgUrl + '">';
 }
 
-function playAudio() {
-  const audio = document.querySelector('audio')
-  audio.addEventListener('play', setImagesInTime);
-}
-
-function showImages(){
-  const para = document.querySelector('p');
-  para.innerHTML = setImageInTime;
-}
-
+// This enables the function when click the audio
 const audio = document.querySelector('audio')
 audio.addEventListener('play', setImagesInTime);
+// audio.addEventListener('pause', HandlePause);
+
+// function HandlePause() {}
+
+
+
+
+
+
+
 
 //button.addEventListener('click', showImages, playAudio);
 
